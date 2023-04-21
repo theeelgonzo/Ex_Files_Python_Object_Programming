85a51610a585388f1328bd5232a5236c8ce76ac8
Jordan Godwin <jordan.godwin13@gmail.com>
2023-04-21 13:15:23 -0500

    Add code to multiple inheritance file.

--- Exercise Files/Ch 2/multiple_start.py
+++ Exercise Files/Ch 2/multiple_start.py
@@ -6,17 +6,24 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
-
+        self.name = "Class A"
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
-
+        self.name = "Class B"
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def showProps(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
+
 
 c = C()
+c.showProps()
+print(C.__mro)
